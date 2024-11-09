7e9b59535175aa75896618929f289e4fd99038bd
Rebelzob <karimdavid07@gmail.com>
2024-11-09 13:44:11 +0000

    Update schema version and remove unused indexes from loans and payments tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_08_181121) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_08_181920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,7 +38,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_08_181121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_loans_on_client_id"
-    t.index ["status"], name: "index_loans_on_status"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -53,7 +52,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_08_181121) do
     t.datetime "updated_at", null: false
     t.index ["loan_id"], name: "index_payments_on_loan_id"
     t.index ["user_id"], name: "index_payments_on_user_id"
-    t.index ["payment_date"], name: "index_payments_on_payment_date"
   end
 
   create_table "users", force: :cascade do |t|
