d75acd368737a171a1a59f6ee76f2b5fcd47fa39
Rebelzob <karimdavid07@gmail.com>
2024-11-10 23:06:14 +0000

    Add user seeding script with Faker for test data generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+require 'faker'
+
+# Clear existing users
+puts "Clearing existing users..."
+User.destroy_all
+
+# Create 100 users
+puts "Creating 100 users..."
+100.times do |i|
+  User.create!(
+    name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone,
+    address: Faker::Address.full_address,
+    comments: Faker::Lorem.paragraph(sentence_count: 2)
+  )
+  print "." if (i + 1) % 10 == 0
+end
+
+puts "\nCreated #{User.count} users!"
