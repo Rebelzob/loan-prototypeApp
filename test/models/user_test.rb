1a0c5c31cc9cba74212226d57b876723d2b698c4
Rebelzob <karimdavid07@gmail.com>
2024-11-12 20:24:06 +0000

    Add user model tests for valid and invalid user creation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save valid user" do
+    user = User.new(
+      name: "Test",
+      last_name: "User",
+      email: "test@example.com",
+      phone: "1234567890",
+      address: "123 Main St",
+      comments: "Test comments"
+    )
+    assert user.save
+  end
+
+  test "should not save user without name" do
+    user = User.new(last_name: "User")
+    assert_not user.save
+  end
 end
