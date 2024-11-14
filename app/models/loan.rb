f3fe8722a11e759ba49f72d0d5bebad7d3428581
Rebelzob <karimdavid07@gmail.com>
2024-11-14 21:25:39 +0000

    Add associations for payments and date_payments in Loan model

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,5 @@
 class Loan < ApplicationRecord
   belongs_to :client, class_name: "User", foreign_key: "client_id"
+  has_many :payments, dependent: :destroy
+  has_many :date_payments, dependent: :destroy
 end
