ee589a1b96d2980861c620ce6fddf9632d79ce51
Rebelzob <karimdavid07@gmail.com>
2024-11-14 21:25:34 +0000

    Update DatePayment model to change client association and add loan association

--- app/models/date_payment.rb
+++ app/models/date_payment.rb
@@ -1,3 +1,4 @@
 class DatePayment < ApplicationRecord
-  belongs_to :client, foreign_key: "user_id", class_name: "User"
+  belongs_to :client, class_name: "User", foreign_key: "client_id"
+  belongs_to :loan
 end
