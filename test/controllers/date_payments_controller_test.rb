5bc865a0e4ace16224f6830c3aef2143f33199fa
Rebelzob <karimdavid07@gmail.com>
2024-11-15 00:34:38 +0000

    Add user and loan associations to date_payment creation and update tests

--- test/controllers/date_payments_controller_test.rb
+++ test/controllers/date_payments_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class DatePaymentsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @date_payment = date_payments(:one)
+    @user = users(:one)
+    @loan = loans(:one)
   end
 
   test "should get index" do
@@ -17,7 +19,14 @@ class DatePaymentsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create date_payment" do
     assert_difference("DatePayment.count") do
-      post date_payments_url, params: { date_payment: {} }
+      post date_payments_url, params: { date_payment: { 
+        user_id: @user.id, 
+        loan_id: @loan.id, 
+        collection_id: 1, 
+        payment_due_date: "2024-12-01", 
+        loan_start_date: "2024-11-01", 
+        final_payment_date: "2025-11-01" 
+      } }
     end
 
     assert_redirected_to date_payment_url(DatePayment.last)
@@ -34,8 +43,12 @@ class DatePaymentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update date_payment" do
-    patch date_payment_url(@date_payment), params: { date_payment: {} }
+    patch date_payment_url(@date_payment), params: { date_payment: { 
+      payment_due_date: "2024-12-15" 
+    } }
     assert_redirected_to date_payment_url(@date_payment)
+    @date_payment.reload
+    assert_equal "2024-12-15", @date_payment.payment_due_date.to_s
   end
 
   test "should destroy date_payment" do
