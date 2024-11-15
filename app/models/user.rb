de92591654ac491a501bfdd897f719cbb888e2cb
Rebelzob <karimdavid07@gmail.com>
2024-11-15 00:34:27 +0000

    Refactor user model to remove client_id foreign key and add presence validations for name, last_name, and phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :loans, foreign_key: "client_id", dependent: :destroy
-  has_many :date_payments, foreign_key: "client_id", dependent: :destroy
+  has_many :loans, dependent: :destroy
+  has_many :date_payments, dependent: :destroy
   has_many :payments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
 end
