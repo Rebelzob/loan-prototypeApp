330baa647fecd17b61adc1d183f42ec01c3b8c33
Rebelzob <karimdavid07@gmail.com>
2024-11-15 00:34:19 +0000

    Refactor loan model to use user association instead of client

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,5 +1,6 @@
 class Loan < ApplicationRecord
-  belongs_to :client, class_name: "User", foreign_key: "client_id"
+  belongs_to :user
   has_many :payments, dependent: :destroy
   has_many :date_payments, dependent: :destroy
+
 end
